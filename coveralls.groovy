b745ab0ee7d82d022d6d5efdff1841c67f6e9855
Yosiya Hinosawa <stibium121@gmail.com>
2013-09-22 23:36:00 +0900

    Implement report json creation

--- coveralls.groovy
+++ coveralls.groovy
@@ -3,7 +3,7 @@
 
 import groovy.json.*
 
-api = 'https://coveralls.io/api/v1/jobs'
+API = 'https://coveralls.io/api/v1/jobs'
 
 // model for coveralls io report's source file report
 class SourceReport {
@@ -28,9 +28,9 @@ class Report {
     public Report() {
     }
 
-    public Report(String serviceJobId, String serviceName, List<SourceReport> sourceFiles) {
-        this.service_job_id = serviceJobId;
+    public Report(String serviceName, String serviceJobId, List<SourceReport> sourceFiles) {
         this.service_name = serviceName;
+        this.service_job_id = serviceJobId;
         this.source_files = sourceFiles;
     }
 
@@ -41,18 +41,74 @@ class Report {
     }
 }
 
-class ReportFactory {
-    public static createTest() {
-        return new Report('123', 'travis-ci', [new SourceReport('abc', 'def', [1, 2, null])])
-    }
+class SourceReportFactory {
 
-    public static createFromCoberturaXML() {
+    public static List<SourceReport> createFromCoberturaXML(File file) {
+        Node coverage = new XmlParser().parse(file)
+        String sourceDir = coverage.sources.source.text() + '/'
+
+        Map a = [:]
+
+        coverage.packages.package.classes.class.each() {
+            Map cov = a.get(it.'@filename', [:])
+
+            it.lines.line.each() {
+                cov[it.'@number'.toInteger()] = it.'@hits'.toInteger()
+            }
+        }
+
+        List<SourceReport> reports = new ArrayList<SourceReport>()
+
+        a.each { String filename, Map cov ->
+            def max = cov.max { it.key }
+
+            List r = [null] * max.key
+            cov.each { Integer line, Integer hits ->
+                r[line] = hits
+            }
+
+            reports.add new SourceReport(filename, new File(sourceDir + filename).text, r)
+        }
+
+        return reports
+
     }
 }
 
+// model for ci service info
+class ServiceInfo {
+    String serviceName;
+    String serviceJobId;
+
+    public ServiceInfo(String serviceName, String serviceJobId) {
+        this.serviceName = serviceName;
+        this.serviceJobId = serviceJobId;
+    }
+}
+
+class ServiceInfoFactory {
+
+    public static createFromEnvVar() {
+
+        if (System.getenv('TRAVIS') == 'true') {
+            return new ServiceInfo('travis-ci', System.getenv('TRAVIS_JOB_ID'))
+        }
+
+        // cannot create service info from env var
+        return null
+
+    }
+
+}
+
 def main() {
-    def a = ReportFactory.createTest()
-    println a.toJson()
+    ServiceInfo serviceInfo = ServiceInfoFactory.createFromEnvVar()
+
+    List<SourceReport> sourceReports = SourceReportFactory.createFromCoberturaXML new File('sample.xml')
+
+    Report rep = new Report(serviceInfo.serviceName, serviceInfo.serviceJobId, sourceReports)
+
+    println rep.toJson()
 }
 
 main()
